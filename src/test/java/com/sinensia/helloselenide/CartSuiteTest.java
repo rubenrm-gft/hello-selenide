7d36a836933cb40cabc6a53cff68b11ac71f7084
Javier Rodriguez <profesor@sinensia.com>
2022-02-14 13:46:54 +0100

    Request video recording capability

--- src/test/java/com/sinensia/helloselenide/CartSuiteTest.java
+++ src/test/java/com/sinensia/helloselenide/CartSuiteTest.java
@@ -20,6 +20,7 @@ public class CartSuiteTest {
 
         DesiredCapabilities capabilites = new DesiredCapabilities();
         capabilites.setCapability("enableVNC", true);
+        capabilites.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilites;
     }
 
